fc08df59785dd3a52a56291c56cda5f89b159e2a
cris <cr.barrales@uandresbello.edu>
2017-06-26 19:28:30 -0400

    proyecto

---
+++ adopcion/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from adopcion.models import Persona
+
+admin.site.register(Persona)
+
+# Register your models here.
---
+++ mascota/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from adopcion.models import Persona	
+
+class Vacuna(models.Model):
+	nombre = models.CharField(max_length=50)
+
+class Mascota(models.Model):
+	folio = models.CharField(max_length=10, primary_key=True)
+	nombre = models.CharField(max_length=50)
+	sexo = models.CharField(max_length=10)
+	edad_aproximada = models.IntegerField()
+	fecha_rescate= models.DateTimeField()
+	persona = models.ForeignKey(Persona, null= True, blank= True,on_delete=models.CASCADE)
+	vacuna = models.ManyToManyField(Vacuna)
