fbed49acedcf5b8d2fb467a549e17e8095ec11da
cris <cr.barrales@uandresbello.edu>
2017-06-27 01:02:23 -0400

    agregando archivos

--- mascota/models.py
+++ mascota/models.py
@@ -5,11 +5,14 @@ from adopcion.models import Persona
 class Vacuna(models.Model):
 	nombre = models.CharField(max_length=50)
 
+	def __str__(self):
+		return '{}'.format(self.nombre)
+
+
 class Mascota(models.Model):
-	folio = models.CharField(max_length=10, primary_key=True)
 	nombre = models.CharField(max_length=50)
 	sexo = models.CharField(max_length=10)
 	edad_aproximada = models.IntegerField()
-	fecha_rescate= models.DateTimeField()
-	persona = models.ForeignKey(Persona, null= True, blank= True,on_delete=models.CASCADE)
-	vacuna = models.ManyToManyField(Vacuna)
+	fecha_rescate = models.DateField()
+	persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
+	vacuna = models.ManyToManyField(Vacuna, blank=True)
