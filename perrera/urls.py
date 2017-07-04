0cbec239d0f795a503c597ef31517efca56cc28f
crbarrales <cr.barrales@uandresbello.edu>
2017-07-03 20:08:37 -0400

    Update urls.py

--- perrera/urls.py
+++ perrera/urls.py
@@ -1,21 +1,10 @@
-"""perrera URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^mascota/', include('mascota.urls', namespace='mascota')),
+    url(r'^adopcion/', include('adopcion.urls', namespace='adopcion')),
+    url(r'^usuario/', include('usuario.urls', namespace='usuario')),
 ]
